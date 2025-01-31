5c31cf4ae75e216f3ebf1f6a4a94804df76c4b4c
lavishabhatia <lavishabhatia1012@gmail.com>
2025-01-31 22:58:21 +0530

    feat: auth header configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
-function lancer({ signingSecret }: { signingSecret: string }) {
-    function auth() {}
+import e from 'express'
+type AuthHandler = ({
+    token,
+    session,
+}: {
+    token: string
+    session: lancerTypes.SessionRequest
+}) => Promise<{ ownerId: string; status: number }>
 
-    function webhook() {}
+function lancer({ signingSecret }: { signingSecret: string }) {
+    function auth(handler: AuthHandler) {
+        return async (
+            req: e.Request,
+            res: e.Response,
+            next: e.NextFunction,
+        ) => {
+            const authHeader = req.headers?.authorization?.split(' ')[1]
+
+            if (!authHeader) {
+                return res.status(403).send()
+            }
+            const body = req.body as lancerTypes.SessionRequest
+            if (
+                body.chunk_size &&
+                body.file_name &&
+                body.file_size &&
+                body.max_chunk &&
+                body.max_chunk &&
+                body.mime_type &&
+                body.provider
+            ) {
+                const ack = await handler({ token: authHeader, session: body })
+                return res.status(ack.status).send({ ownerId: ack.ownerId })
+            } else {
+                return res.status(422).send()
+            }
+        }
+    }
+
+    function webhook() {
+        return (req: e.Request, res: e.Response, next: e.NextFunction) => {}
+    }
 
     return {
         auth,
