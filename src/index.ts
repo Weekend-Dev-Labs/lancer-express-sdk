4514611fd8f6d70eef576da33350bf3bcd0b8abf
lavishabhatia <lavishabhatia1012@gmail.com>
2025-02-02 18:32:32 +0530

    feat added webhook handler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import e from 'express'
+import crypto from 'crypto'
+
 type AuthHandler = ({
     token,
     session,
@@ -6,6 +8,30 @@ type AuthHandler = ({
     token: string
     session: lancerTypes.SessionRequest
 }) => Promise<{ ownerId: string; status: number }>
+
+type WebHookHandler = ({
+    event,
+    payload,
+}: {
+    event: lancerTypes.Event
+    payload: lancerTypes.WebhookEvent<lancerTypes.Session | lancerTypes.UFile>
+}) => Promise<boolean>
+
+function verifySignature(
+    payload: string,
+    timestamp: string,
+    signature: string,
+    secret: string,
+) {
+    const message = `${timestamp}.${payload}`
+    const hmac = crypto.createHmac('sha256', secret)
+    hmac.update(message)
+    const expectedSignature = hmac.digest('hex')
+    return crypto.timingSafeEqual(
+        Buffer.from(signature),
+        Buffer.from(expectedSignature),
+    )
+}
 
 function lancer({ signingSecret }: { signingSecret: string }) {
     function auth(handler: AuthHandler) {
@@ -37,8 +63,43 @@ function lancer({ signingSecret }: { signingSecret: string }) {
         }
     }
 
-    function webhook() {
-        return (req: e.Request, res: e.Response, next: e.NextFunction) => {}
+    function webhook(handler: WebHookHandler, verification?: boolean) {
+        return async (
+            req: e.Request,
+            res: e.Response,
+            next: e.NextFunction,
+        ) => {
+            if (verification) {
+                const sig = req.headers['x-signature']
+                const timestamp = req.headers['x-timestamp']
+                if (sig && timestamp) {
+                    const body = JSON.stringify(req.body)
+                    const isVerified = verifySignature(
+                        body,
+                        timestamp as string,
+                        sig as string,
+                        signingSecret,
+                    )
+                    if (!isVerified) {
+                        return res.status(400).send()
+                    }
+                    
+                } else {
+                    return res.status(400).send()
+                }
+            }
+            const ack = await handler({
+                event: req.body.event,
+                payload: req.body.data,
+            })
+            if (ack) {
+                res.status(200).send()
+                return
+            } else {
+                res.status(400).send()
+                return
+            }
+        }
     }
 
     return {
